2e81d6fc42c051d9b4f508b8073fa88c456d2d71
Joakim Olofsson <joakim.olofsson@iths.se>
2024-09-27 16:40:13 +0200

    Constructor, properties and Expression-Bodied Members

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,14 +4,98 @@
     {
         static void Main(string[] args)
         {
-            CarOwner carOwner = new CarOwner();
+            /*CarOwner carOwner = new CarOwner();
             Console.WriteLine(carOwner.carA.manufacturer);
             Console.WriteLine($"Current speed is: {carOwner.carA.currentSpeed} km/h! Change the speed:");
-            carOwner.carA.GoFaster(Console.ReadLine());
+            carOwner.carA.GoFaster(Console.ReadLine());*/
+
+            /*Person person1 = new Person("Joakim", "Olofsson", 37, "Gatan");*/
+
+            /*Car car1 = new Car("Volvo", "V70", "Silver");
+            Console.WriteLine($"Tillverkare: {car1.manufacturer}\n" +
+                $"Modell: {car1.model}\n" +
+                $"Färg: {car1.color}\n" +
+                $"Hastighet: {car1.ChangeSpeed()}");*/
+
+            Person person1 = new Person("Joakim", "Olofsson");
+            Console.WriteLine(person1.FullName);
         }
     }
 
-    class CarA
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = HandleInput(value);
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = HandleInput(value);
+        }
+        public string FullName { get => $"{_firstName} {_lastName}"; }
+
+        private string HandleInput(string value)
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                return "Dont leave this empty!";
+            }
+
+            return value;
+        }
+    }
+
+    /*class Car
+    {
+        public string manufacturer;
+        public string model;
+        public string color;
+        private int _speed;
+        private int _maxSpeed;
+
+        public Car(string manufacturer, string model, string color)
+        {
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.color = color;
+            _speed = 50;
+            _maxSpeed = 200;
+        }
+
+        public int ChangeSpeed()
+        {
+            return _speed = 100;
+        }
+    }*/
+
+    /*class Person
+    {
+        public string name;
+        public string lastname;
+        public int age;
+        public string address;
+
+        public Person(string name, string lastname, int age, string address)
+        {
+            name = name;
+            lastname = lastname;
+            age = age;
+            address = address;
+        }
+    }*/
+
+    /*class CarA
     {
         public string manufacturer = "Volvo";
         public string model = "V70";
@@ -33,9 +117,9 @@
                 Console.WriteLine("The car can't go that fast!");
             }
         }
-    }
+    }*/
 
-    class CarB
+    /*class CarB
     {
         public string manufacturer = "Audi";
         public string model = "A3";
@@ -57,11 +141,11 @@
                 Console.WriteLine("The car can't go that fast!");
             }
         }
-    }
+    }*/
 
-    class CarOwner
+    /*class CarOwner
     {
         public CarA carA = new CarA();
         public CarB carB = new CarB();
-    }
+    }*/
 }
